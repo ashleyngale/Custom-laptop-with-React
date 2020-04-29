d9bc14d139b1f2bc72203f1fe599037d44e994a7
Ashley Gale <ashleyygale@gmail.com>
2020-04-29 11:21:07 -0400

    Fix files

---
+++ src/CartItems.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React, { Component } from 'react';
+
+//add new cart items
+updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
+
+  //useres selected cart items
+  const summary = Object.keys(this.state.selected).map((feature, idx) => {
+    const featureHash = feature + '-' + idx;
+    const selectedOption = this.state.selected[feature];
+
+    return (
+      <div className="summary__option" key={featureHash}>
+        <div className="summary__option__label">{feature} </div>
+        <div className="summary__option__value">{selectedOption.name}</div>
+        <div className="summary__option__cost">
+          {USCurrencyFormat.format(selectedOption.cost)}
+        </div>
+      </div>
+    );
+  });
+
+  const total = Object.keys(this.state.selected).reduce(
+    (acc, curr) => acc + this.state.selected[curr].cost,
+    0
+  );
+
+  //calculate cart items
+  const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
