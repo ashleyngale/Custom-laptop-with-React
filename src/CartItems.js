c76cf2c8b12f8a307f9c79f90d681132d2a941c9
Ashley Gale <ashleyygale@gmail.com>
2020-04-30 12:00:24 -0400

    Import comps

--- src/CartItems.js
+++ src/CartItems.js
@@ -15,7 +15,6 @@ return (
     </div>
   </div>
 );
-
 //add new cart items
 updateFeature = (feature, newValue) => {
   const selected = Object.assign({}, this.state.selected);
@@ -25,11 +24,13 @@ updateFeature = (feature, newValue) => {
   });
 };
 
-//useres selected cart items
+class CartSummary extends Component{
+  render(){
+    return(
 const summary = Object.keys(this.state.selected).map((feature, idx) => {
   const featureHash = feature + '-' + idx;
   const selectedOption = this.state.selected[feature];
-});
+
 
 const total = Object.keys(this.state.selected).reduce(
   (acc, curr) => acc + this.state.selected[curr].cost,
@@ -40,7 +41,8 @@ const total = Object.keys(this.state.selected).reduce(
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD'
-});
-}
+
+};
+  };
 }
 export default CartItems;
