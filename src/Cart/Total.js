b23865246058c58ebf596d4bb0c7fe6343aa5f9e
Ashley Gale <ashleyygale@gmail.com>
2020-06-11 16:25:12 -0400

    Console total error

--- src/Cart/Total.js
+++ src/Cart/Total.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 
 export default class Total extends Component {
     render(props){
-        const total = Object.keys(this.props.selected).reduce(
-			(acc, curr) => acc + this.props.selected[curr].cost,
-			0
-		);
+        console.log(this.props.selected)
+        const total = (Object.values(this.props.selected)).reduce(
+            (acc, curr) => acc + curr.cost,
+            0
+        );
         return (
             <div className="summary__total__value">
                 {this.props.USCurrencyFormat.format(total)}
